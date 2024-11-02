e984659d0ffe13f289c08f2d1fa46c11f61083fd
p1k0chu <anatoliyid0207@gmail.com>
2024-11-02 22:46:34 +0200

    new project from the template

---
+++ src/client/kotlin/com/github/p1k0chu/mc_mod/bacap_overview/BACAPOverviewClient.kt
@@ -0,0 +1,9 @@
+package com.github.p1k0chu.mc_mod
+
+import net.fabricmc.api.ClientModInitializer
+
+object BACAPOverviewClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
