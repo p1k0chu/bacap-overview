bec085ffd555285f5a5ee7d180ad5dad97e2cdc5
p1k0chu <anatoliyid0207@gmail.com>
2024-11-03 20:27:35 +0200

    add common side Mod Initializer
    also fix client sided initialized 'package'

--- src/client/kotlin/com/github/p1k0chu/mc_mod/bacap_overview/BACAPOverviewClient.kt
+++ src/client/kotlin/com/github/p1k0chu/mc_mod/bacap_overview/BACAPOverviewClient.kt
@@ -1,9 +1,9 @@
-package com.github.p1k0chu.mc_mod
+package com.github.p1k0chu.mc_mod.bacap_overview
 
 import net.fabricmc.api.ClientModInitializer
 
 object BACAPOverviewClient : ClientModInitializer {
 	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
 	}
 }
---
+++ src/main/kotlin/com/github/p1k0chu/mc_mod/bacap_overview/BACAPOverview.kt
@@ -0,0 +1,9 @@
+package com.github.p1k0chu.mc_mod.bacap_overview
+
+import net.fabricmc.api.ModInitializer
+
+object BACAPOverview : ModInitializer {
+    override fun onInitialize() {
+
+    }
+}
